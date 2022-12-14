abd5aee909e8ec0d59e895a6f97bd732895abce3
Hiram <dsbzwh@gmail.com>
2022-12-14 04:20:54 -0600

    Antes de agregar comando rm-rf

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from pytube.cli import on_progress
 from moviepy.editor import *
 import os
 
-def download(video, vid_index):
-    
+def download(video, vid_index, waitlist_id=0):
+    waitlist_id = str(waitlist_id)
     video_stream = video.streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').desc()[vid_index]
     audio_stream = video.streams.filter(adaptive=True, file_extension="mp4", only_audio=True).order_by('abr').desc().first()
     vid_size = str(int((video_stream.filesize/1024/1024)))
@@ -14,27 +14,29 @@ def download(video, vid_index):
     try:
         print("Descargando video... " + vid_name)
         print("Tamaño: " + vid_size + " MB\n")
-        video_stream.download(output_path="./media/", filename="video.mp4")
+        video_stream.download(output_path="./media/videos/", filename=waitlist_id + ".mp4")
         all_good[0] = True
     except:
         print("Error al descargar el video")
     try:
         print("Descargando audio...")
-        audio_stream.download(output_path="./media/", filename="audio.mp4")
+        audio_stream.download(output_path="./media/audios/", filename=waitlist_id + ".mp4")
         all_good[1] = True
     except:
         print("Error al descargar el audio")
-    
     if all_good[0] and all_good[1]:
+        #Se asegura de que el video termine en la carpeta downloads
         vid_name = vid_name + ".mp4"
         vid_name = "./downloads/" + vid_name
+        #Se asegura de exista lel directorio de descarga
         if os.path.isdir("./downloads") != True:
             os.mkdir("./downloads")
-        combine("./media/video.mp4", "./media/audio.mp4", vid_name)
+        all_good = True
     else:
         print("Lo siento, hubo un error al obtener la información del video")
-
-#Muestra info sobre el video, resoluciones y tamaño máximo en MB
+        all_good = False
+    return all_good
+    
 def get_info(link):
     video = yt(link, on_progress_callback=on_progress)
     video_streams = video.streams.filter(adaptive=True, file_extension="mp4").order_by("resolution").desc()
@@ -58,22 +60,46 @@ def decide_res(video, available_streams):
             print("Sólo puedes ingresar números")
     choice = choice -1
     return choice
+
+def filter_res():
+    qualities = []
+    itags = {}
+    exists = False
+    for stream in video_streams:
+        for quality in qualities:
+            if stream.resolution == quality:
+                exists = True
+        if exists == False:
+            itags[stream.itag] = stream.resolution
+            qualities.append(stream.resolution)
+        else:
+            exists = False
             
 def combine(video,audio,output_name):
     os.system("clear")
-    print("Espera un momento mientras convertimos tu video a la mejor calidad :)")
+    print("Espera un momento mientras convertimos tu video a la mejor calidad :)\n")
     video_clip = VideoFileClip(video)
     audio_clip = AudioFileClip(audio)
     new_audio_clip = CompositeAudioClip([audio_clip])
     video_clip.audio = new_audio_clip
-    video_clip.write_videofile(output_name, codec="libx264")
-    print("Video convertido :)")
-    
+    try:
+        video_clip.write_videofile(output_name, codec="libx264")
+        print("Video descargado y convertido exitosamente!")
+    except:
+        print("Hubo un error durante la conversión")
+    os.system("rm -rf /media")
+
 def run():
     url = input("Pega el enlace al video: ")
     video, video_streams = get_info(url)
     choice = decide_res(video, video_streams)
-    download(video, choice)
+    all_good = download(video, choice)
+    title = "/downloads/" + video.title + ".mp4"
+    if all_good:
+        ##Arreglar: No funciona para convertir videos con / en el título
+        combine("./media/videos/0.mp4", "./media/audios/0.mp4", title)
+    else:
+        "Intenta de nuevo"
     
 
 if __name__ == "__main__":
